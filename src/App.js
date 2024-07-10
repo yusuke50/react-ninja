f2ad9f35adecafe3d64144d868cc3cda70990770
yusuke050 <yusuke050@gmail.com>
2024-07-10 08:30:03 +0800

    lesson 1-6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const likes = 50;
+  const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+  };
+  const link = 'http://www.google.com';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Navbar />
+
+      <div className='content'>
+        <Home />
+        <p>Liked {likes} times</p>
+
+        <p>{10}</p>
+        <p>{'hello'}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {person.firstName} {person.lastName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <a href={link}>Google</a>
+      </div>
     </div>
   );
 }
