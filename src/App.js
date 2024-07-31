d44ff30dca05106565980787626649c26987a6ea
yusuke050 <yusuke050@gmail.com>
2024-07-31 22:23:43 +0800

    react-2nd

--- src/App.js
+++ src/App.js
@@ -2,30 +2,12 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const likes = 50;
-  const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-  };
-  const link = 'http://www.google.com';
-
   return (
     <div className='App'>
       <Navbar />
 
       <div className='content'>
         <Home />
-        <p>Liked {likes} times</p>
-
-        <p>{10}</p>
-        <p>{'hello'}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>
-          {person.firstName} {person.lastName}
-        </p>
-
-        <a href={link}>Google</a>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, []);
+
   return (
-    <div>
-      <h2>Homepage</h2>
+    <div className='home'>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All Blogs' />}
     </div>
   );
 };
